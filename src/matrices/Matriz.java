0d2ac8388335e24c2fa0f3ad8201fe226eb78154
javierre <javierperezlara@gmail.com>
2018-08-03 16:52:14 +0200

    merge de ambas ramas

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -49,6 +49,7 @@ public class Matriz {
         return matrizResultante;
     } 
 
+<<<<<<< HEAD
     
     public static Matriz invertirMatriz(Matriz a){ 
         int i, j, filasA, columnasA; 
@@ -65,6 +66,27 @@ public class Matriz {
 
     
     
+=======
+    public static Matriz multiplicarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles { 
+        int i, j, k, filasA, columnasA, filasB, columnasB; 
+        filasA = a.getDimension().height; 
+        columnasA = a.getDimension().width; 
+        filasB = b.getDimension().height; 
+        columnasB = b.getDimension().width;
+        if(columnasA!=filasB) throw new DimensionesIncompatibles("El producto de matrices requiere que la primera sea de tamaño nxp y la segunda pxm.");        
+        
+        Matriz matrizResultante = new Matriz(filasA, columnasB, false);
+        for (k = 0; k < columnasB; k++) { 
+            for (i = 0; i < filasA; i++) { 
+                for (j = 0; j < columnasA; j++) { 
+                    matrizResultante.datos[i][k] += a.datos[i][j] * b.datos[j][k]; 
+                } 
+            } 
+        }
+        return matrizResultante;
+    } 
+
+>>>>>>> Perez_Multiplicar
     @Override
     public String toString(){
         String ret = "";
